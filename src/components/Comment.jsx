c433cd4697df1440c1f94bad21add2388b1e6d3a
João Phillipe Fernandes <joao.phillipe@ufba.br>
2024-02-02 02:22:33 -0300

    validation

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,13 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment({content}){
+export function Comment({content, onDeleteComment}){
+
+    function handleDeleteComment(){
+        onDeleteComment(content);
+    }
+
+
     return(
         <div className={styles.comment}>
             <Avatar hasBorder = {false} src="https://avatars.githubusercontent.com/u/92993184?v=4"/>
@@ -14,7 +20,7 @@ export function Comment({content}){
                             <time dateTime='2024-01-22 12:37:00' >Cerca de 1h atrás</time>
                         </div>
 
-                        <button title='Deletar Comentários'>
+                        <button onClick={handleDeleteComment} title='Deletar Comentários'>
                             <Trash size={24}/>
                         </button>
                     </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -11,7 +11,7 @@ import { useState } from 'react';
 
 export function Post({author, publishedAt, content}){
     const [comments, setComments] = useState( [
-        'Boa!'
+        "Oi"
     ])
 
     const [newCommentText, setNewCommentText] = useState('')
@@ -35,8 +35,20 @@ export function Post({author, publishedAt, content}){
     }
 
     function handleNewCommentChange(e) {
+        event.target.setCustomValidity('')
         setNewCommentText(e.target.value );
         
+    }
+
+    function deleteComment(commentToDelete){
+        const newCommentList = comments.filter(comment=>{
+            return comment != commentToDelete;
+        })
+        setComments(newCommentList);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Campo Obrigatório')
     }
 
     
@@ -59,9 +71,9 @@ export function Post({author, publishedAt, content}){
         <div className={styles.content}>
             {content.map(line =>{
                 if (line.type === 'paragraph'){
-                    return <p>{line.content}</p>
+                    return <p key={line.content}>{line.content}</p>
                 }   else if (line.type ==='link'){
-                    return <p><a href="#">{line.content}</a></p>
+                    return <p key={line.content}><a href="#">{line.content}</a></p>
                 }
             })}
         </div>
@@ -73,16 +85,18 @@ export function Post({author, publishedAt, content}){
                 value={newCommentText}
                 placeholder='Deixe seu comentário'  
                 onChange={handleNewCommentChange} 
+                onInvalid={handleNewCommentInvalid}
+                required
             />
             <footer>
-                <button type='submit'>Publicar</button>
+                <button type='submit' disabled= {newCommentText.length===0}>Publicar</button>
             </footer>
         </form>
 
         <div className={styles.commentList}>
            {comments.map((comment)=>{
                 
-               return <Comment content= {comment}/>
+               return <Comment key={comment} content= {comment} onDeleteComment= {deleteComment}/>
            })}
         </div>
     </article>
